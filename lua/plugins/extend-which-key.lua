c237df95df6d7a99682e7a800f638c1166be3a8f
WhateverMO_rog <75033001+WhateverMO@users.noreply.github.com>
2025-04-13 18:18:02 +0800

    which-key preset to modern

---
+++ lua/plugins/extend-which-key.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    preset = "modern",
+    -- layout = {
+    --   width = { min = 20, max = 90 }, -- min and max width of the columns
+    --   spacing = 3, -- spacing between columns
+    -- },
+  },
+}
